67fb38782a52d1c8faf2e6c837be3637276eda2c
aksarlaode <akhsaras21@gmail.com>
2024-07-25 12:44:25 +0000

    rename

--- apps/expo/eslint.config.mjs
+++ apps/expo/eslint.config.mjs
@@ -1,5 +1,5 @@
-import baseConfig from "@acme/eslint-config/base";
-import reactConfig from "@acme/eslint-config/react";
+import baseConfig from "@aksar/eslint-config/base";
+import reactConfig from "@aksar/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
--- apps/nextjs/src/app/_components/posts.tsx
+++ apps/nextjs/src/app/_components/posts.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import type { RouterOutputs } from "@acme/api";
-import { CreatePostSchema } from "@acme/db/schema";
-import { cn } from "@acme/ui";
-import { Button } from "@acme/ui/button";
+import type { RouterOutputs } from "@aksar/api";
+import { CreatePostSchema } from "@aksar/db/schema";
+import { cn } from "@aksar/ui";
+import { Button } from "@aksar/ui/button";
 import {
   Form,
   FormControl,
@@ -11,9 +11,9 @@ import {
   FormItem,
   FormMessage,
   useForm,
-} from "@acme/ui/form";
-import { Input } from "@acme/ui/input";
-import { toast } from "@acme/ui/toast";
+} from "@aksar/ui/form";
+import { Input } from "@aksar/ui/input";
+import { toast } from "@aksar/ui/toast";
 
 import { api } from "~/trpc/react";
 
